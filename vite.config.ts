f9d2128f6826833b5f106ed33f9602dc84674a81
Daniel Rod <dsrod@seznam.cz>
2022-05-14 21:44:40 +0200

    update base path for GH pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, ConfigEnv } from 'vite'
 import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [preact()],
-  resolve: {
-    alias: {
-      "react": "preact/compat",
-      "react-dom/test-utils": "preact/test-utils",
-      "react-dom": "preact/compat",
-      "react/jsx-runtime": "preact/jsx-runtime"
+export default defineConfig(({ command, mode }: ConfigEnv) => {
+  const base_config = {
+    plugins: [preact()],
+    resolve: {
+      alias: {
+        "react": "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+        "react/jsx-runtime": "preact/jsx-runtime"
+      }
     }
   }
+  if (command === 'build')
+  {
+    return {...base_config, base: "/menuet/"}
+  }
+  return base_config
 })
