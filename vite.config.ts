a6b83120787921b81ab8ab2763cfb6ea742ea87e
dsrod <dsrod@seznam.cz>
2022-05-16 00:15:35 +0200

    Generate PWA files, use new favicon

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,30 @@ import preact from '@preact/preset-vite'
 export default defineConfig(({ command, mode }: ConfigEnv) => {
   const base_config = {
     plugins: [preact(), VitePWA({
-      includeAssets: ['favicon.png'],
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
       manifest: {
         name: 'Menuet',
         short_name: 'Menuet',
         description: 'Keeping track of all the drinks!',
         theme_color: '#673ab8',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          }
+        ],
       },
       registerType: 'autoUpdate',
       devOptions: {
